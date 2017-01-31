3b1f71d034df50f1068ad8eb142d2b711205e081
George Antonious <george@antonious.ca>
2017-01-31 09:12:33 -0700

    Return 404 when trying to get the details of a question that doesn't exist

--- lab4/mysite/polls/views.py
+++ lab4/mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from models import Question
 
@@ -9,7 +9,10 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question {}.".format(question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+
+    return render(request, 'polls/details.html', context)
 
 def results(request, question_id):
     return HttpResponse("You're looking at the results of question {}.".format(question_id))
