aaa25063dfc199cd12286fe7f6a347a6fe92e412
George Antonious <george@antonious.ca>
2017-01-31 08:47:36 -0700

    Work on lab4

---
+++ lab4/mysite/polls/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("You are at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question {}.".format(question_id))
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of question {}.".format(question_id))
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question {}.", question_id)
