c537756afef9c881132ff2f7153827c33e83a3d0
George Antonious <george@antonious.ca>
2017-01-31 08:58:31 -0700

    Fix rendering voting

--- lab4/mysite/polls/views.py
+++ lab4/mysite/polls/views.py
@@ -11,4 +11,4 @@ def results(request, question_id):
     return HttpResponse("You're looking at the results of question {}.".format(question_id))
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question {}.", question_id)
+    return HttpResponse("You're voting on question {}.".format(question_id))
