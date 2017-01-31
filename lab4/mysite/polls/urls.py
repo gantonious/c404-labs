412888ac74f7f63be09a48e9a71d78603bc26c73
George Antonious <george@antonious.ca>
2017-01-31 08:51:38 -0700

    Get basic polls url structure working

--- lab4/mysite/polls/urls.py
+++ lab4/mysite/polls/urls.py
@@ -2,5 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    # /poll/
+    url(r'^$', views.index, name='index'),
+    # /poll/5
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # /poll/5/results
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # /poll/5/vote
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote')
 ]
