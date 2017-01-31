f9faa3488050080aac87c6a65daa69ed951d1ed8
George Antonious <george@antonious.ca>
2017-01-31 10:04:20 -0700

    User generic view classes to render data

--- lab4/mysite/polls/views.py
+++ lab4/mysite/polls/views.py
@@ -1,25 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
 from models import Choice, Question
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-    return render(request, 'polls/index.html', context)
+    def get_queryset(self):
+        return Question.objects.orderby('-pub_date')[:5]
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/details.html'
 
-    return render(request, 'polls/details.html', context)
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
-
-    return render(request, 'polls/results.html', context)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
